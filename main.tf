ecc2185654738aaa14991e806f59cd40f45c376c
Yugandhar Reddy <yreddy.adz@gmail.com>
2018-11-09 09:14:46 -0500

    updated user_data

--- main.tf
+++ main.tf
@@ -2,11 +2,45 @@ provider "aws"{
   region  = "us-east-1"
 }
 
+
 resource "aws_instance" "example"{
-  ami = "ami-40d28157"
+  ami = "ami-06b5810be11add0e2"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.example.id}"]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World!" > index.html
+    nohup busybox httpd -f -p "${var.server_port}" &
+    EOF
 
   tags{
     Name = "terraform-example"
+    env = "dev"
   }
+}
+
+
+#security group
+resource "aws_security_group" "example"{
+  name = "allow 8080"
+
+  ingress{
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    from_port = "22"
+    to_port = "22"
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port"{
+  description="instance webserver port to run on"
+  default = "8080"
 }
